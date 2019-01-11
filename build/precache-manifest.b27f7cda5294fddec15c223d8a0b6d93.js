7720ba3f206ff08c0b645a673f73d03d449da79a
Sebastian Waluś <kozy@op.pl>
2019-01-11 18:50:56 +0100

    added build folder with static app

---
+++ build/precache-manifest.b27f7cda5294fddec15c223d8a0b6d93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f09af2dd863ccbd96e2",
+    "url": "/static/js/main.1f09af2d.chunk.js"
+  },
+  {
+    "revision": "70fde16567094ea2ecc7",
+    "url": "/static/js/1.70fde165.chunk.js"
+  },
+  {
+    "revision": "1f09af2dd863ccbd96e2",
+    "url": "/static/css/main.bde8ae1a.chunk.css"
+  },
+  {
+    "revision": "e094085ac6ef7ca3cf2f7a0bd49c3a87",
+    "url": "/index.html"
+  }
+];
